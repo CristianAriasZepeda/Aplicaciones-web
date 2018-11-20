b314494011f26866331832ec134667d92c83a591
Cris el barbaro <38225625+CristianAriasZepeda@users.noreply.github.com>
2018-11-20 06:09:49 -0600

    Add files via upload
    Login Controller

--- adogcatme/AppData/Controller/loginController.php
+++ adogcatme/AppData/Controller/loginController.php
@@ -1,50 +1,90 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Luis
- * Date: 12/06/2018
- * Time: 01:40 PM
- */
+<?php namespace AppData\Controller;
+    use AppData\Model\Login;
+    class loginController{
+        private $login;
+        function __construct(){
+                $this->login=new Login();
+        }
+        function index(){
 
-namespace AppData\Controller;
-
-
-class loginController
-{
-    private $login;
-
-    public function __construct()
-        {
-            $this->login= new \AppData\Model\Login();
         }
 
-    public function index()
-        {
-            //session_destroy();
+        public function verify() {
+        if(isset($_POST)) {
+            $this->login->set("usuario", $_POST["usuario"]);
+            $this->login->set("contraseña", $_POST["contraseña"]);
+            $datos = $this->login->verify();
+            if (mysqli_num_rows($datos) > 0) {
+                $datos=mysqli_fetch_assoc($datos);
+                $_SESSION["nombre_p"]=$datos["nombre_p"] . " " . $datos["ap_p"] . " " . $datos["am_p"];
+            } else {
+                $_SESSION["error_login"] = "los datos no coinciden con nuestros registros";
+            } ?>
+                        <script type="text/javascript">
+                            //window.location.href = "<?php echo URL?>";
+                        </script>
+                        <?php
         }
-    public function verify()
-        {
-            $_SESSION["error_login"]="";
-
-            if(isset($_POST)) {
-                $this->login->set("email", $_POST["email"]);
-                $this->login->set("pass", $_POST["password"]);
-                $datos = $this->login->verify();
-                if (mysqli_num_rows($datos) > 0) {
-                    $datos=mysqli_fetch_assoc($datos);
-                    $_SESSION["username"]=$datos["email"];
-                    header("Location:" . URL . "Empleado_bienvenido");
-
-                }
-                else {
-                    $_SESSION["error_login"] = "los datos no coinciden con nuestros registros";
-                    header("Location:" . URL . "login");
+    }
+        public function logout(){
+            session_destroy();
+        }
+        public function registrar(){
+            $datos[0]=$this->login->getSex();
+            return $datos;
+        }
+        public function guardar(){
+            if(isset($_POST)){
+                $this->login->set("nombre_p",$_POST['nombre_p']);
+                $this->login->set("ap_p",$_POST['ap_p']);
+                $this->login->set("am_p",$_POST['am_p']);
+                $this->login->set("edad",$_POST['edad']);
+                $this->login->set("id_sexo",$_POST['id_sexo']);
+                $this->login->set("nickname",$_POST['nickname']);
+                $this->login->set("contraseña",$_POST['contraseña']);
+                $this->login->guardar();
+                $dato=$this->login->getOne();
+                if (mysqli_num_rows($dato) > 0) { ?>
+                    <script type="text/javascript">
+                    $(document).ready(function(){
+                        swal({
+                            title : " ",
+                            text : "\nGuardado correctamente",
+                            closeOnCancel :  false,
+                            closeOnConfirm : false,
+                            showCancelButton: false,
+                            showConfirmButton: false,
+                            timer : 2000
+                        });
+                        setTimeout(function(){
+                            window.location.href = "<?php echo URL?>login";
+                        },2100)
+                    })
+                    </script>
+                    <?php
                 }
             }
         }
-    public function logout()
-        {
-            session_destroy();
-           // header("Location:".URL);
+        public function registro(){
+
         }
-}
+        public function guarda(){
+            if(isset($_POST)){
+                $this->login->set("nombre_p",$_POST['nombre_p']);
+                $this->login->set("ap_p",$_POST['ap_p']);
+                $this->login->set("am_p",$_POST['am_p']);
+                $this->login->set("edad",$_POST['edad']);
+                $this->login->set("id_sexo",$_POST['id_sexo']);
+                $this->login->insertaUsuario();
+                ?>
+                <script type="text/javascript">
+                    window.location.href = "<?php echo URL?>login";
+                </script>
+                <?php
+            }
+        }
+        function __destruct(){
+
+        }
+    }
+?>
